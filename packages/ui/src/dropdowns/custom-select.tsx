f75e600a5b56837854a8a82d29fd963b499c1f90
piotrjacak <piotrjac23@gmail.com>
2026-09-09 12:30:29 +0200

    fix

--- packages/ui/src/dropdowns/custom-select.tsx
+++ packages/ui/src/dropdowns/custom-select.tsx
@@ -122,7 +122,7 @@ function CustomSelect(props: ICustomSelectProps) {
             <Combobox.Options data-prevent-outside-click>
               <div
                 className={cn(
-                  "z-30 my-1 min-w-48 overflow-y-scroll rounded-md border-[0.5px] border-subtle-1 bg-surface-1 px-2 py-2.5 text-11 whitespace-nowrap focus:outline-none",
+                  "z-30 my-1 min-w-48 overflow-y-scroll rounded-md border-[0.5px] border-subtle-1 bg-surface-1 py-2.5 text-11 whitespace-nowrap focus:outline-none",
                   optionsClassName
                 )}
                 ref={setPopperElement}
@@ -130,7 +130,7 @@ function CustomSelect(props: ICustomSelectProps) {
                 {...attributes.popper}
               >
                 <div
-                  className={cn("space-y-1 overflow-y-scroll", {
+                  className={cn("vertical-scrollbar scrollbar-xs space-y-1 overflow-y-scroll px-2", {
                     "max-h-60": maxHeight === "lg",
                     "max-h-48": maxHeight === "md",
                     "max-h-36": maxHeight === "rg",
